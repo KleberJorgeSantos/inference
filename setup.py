14bc0ade3175d8dbc5a92329e2e2402027780cbd
KleberJorgeSantos <kleberjorgesantos@gmail.com>
2021-12-01 03:29:37 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,11 @@ VERSION = "0.0.0.1"
 
 setup(
     name='inference',
-    packages=['matcher'],
+    packages=['inference'],
     version=VERSION,
     description='Tecnicas de inferencia',
     author='Kleber Jorge Santos',
     url='https://github.com/benmiroglio/pymatch',
-    download_url='https://github.com/benmiroglio/pymatch/archive/{}.tar.gz'.format(VERSION),
+    download_url='https://github.com/KleberJorgeSantos/inference/{}.tar.gz'.format(VERSION),
     keywords=['logistic', 'regression', 'matching', 'observational', 'study', 'causal', 'inference'],
     include_package_data=True,
-    # install_requires=dependencies
