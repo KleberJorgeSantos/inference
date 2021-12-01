f30ca2b58daef6092758e51f13550bcda3d387ae
KleberJorgeSantos <kleberjorgesantos@gmail.com>
2021-12-01 11:00:59 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ dependencies = [
 VERSION = "0.0.0.1"
 
 setup(
-    name='inference',
-    packages=['inference'],
+    name='pareamento',
+    packages=['pareamento'],
     version=VERSION,
     description='Tecnicas de inferencia',
     author='Kleber Jorge Santos',
     url='https://github.com/KleberJorgeSantos/inference',
-    download_url='https://github.com/KleberJorgeSantos/archive/{}.tar.gz'.format(VERSION),
+    download_url='https://github.com/KleberJorgeSantos/inference/archive/{}.tar.gz'.format(VERSION),
     keywords=['logistic', 'regression', 'matching', 'observational', 'study', 'causal', 'inference'],
     include_package_data=True,
     install_requires=dependencies
