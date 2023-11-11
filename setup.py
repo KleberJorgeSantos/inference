4f7f826b76c35d0a1f5f3bb1cfdc2a0e4964ead3
Kleber Jorge Santos <kleber.santos@sulamerica.com.br>
2023-11-11 02:17:19 -0300

    Atualização da versão

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description='Tecnicas de inferencia',
     author='Kleber Jorge Santos',
     url='https://github.com/KleberJorgeSantos/inference',
-    download_url='https://github.com/KleberJorgeSantos/inference/archive/{}.tar.gz'.format(VERSION),
+    # download_url='https://github.com/KleberJorgeSantos/inference/archive/{}.tar.gz'.format(VERSION),
     keywords=['logistic', 'regression', 'matching', 'observational', 'study', 'causal', 'inference'],
     include_package_data=True,
     install_requires=dependencies
